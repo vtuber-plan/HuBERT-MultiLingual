2ec5dc1eb9336b8690019a0a5f6f7a6ca6b14081
jstzwj88 <110.3870790@qq.com>
2022-09-25 16:34:04 +0800

    update preprocess script

--- pretrain.sh
+++ pretrain.sh
@@ -1,1 +1,21 @@
-fairseq-hydra-train --config-dir "examples/hubert/config/pretrain/" --config-name hubert_base_librispeech
+tsv_dir=./tsv_dir/
+feat_dir=./feat_dir/
+
+# First, use simple_kmeans to generate labels
+
+# feature extraction (MFCC feature)
+python hubert/simple_kmeans/dump_mfcc_feature.py ${tsv_dir} ${split} ${nshard} ${rank} ${feat_dir}
+# feature extraction (HUBERT feature)
+python hubert/simple_kmeans/dump_hubert_feature.py ${tsv_dir} ${split} ${ckpt_path} ${layer} ${nshard} ${rank} ${feat_dir}
+
+# k-means clustering
+python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} ${split} ${nshard} ${km_path} ${n_cluster} --percent 0.1
+# k-means application
+python hubert/simple_kmeans/dump_km_label.py ${feat_dir} ${split} ${km_path} ${nshard} ${rank} ${lab_dir}
+
+for rank in $(seq 0 $((nshard - 1))); do
+  cat $lab_dir/${split}_${rank}_${nshard}.km
+done > $lab_dir/${split}.km
+
+# Then, pretrain the hubert model
+fairseq-hydra-train --config-dir "hubert/config/pretrain/" --config-name hubert_base_librispeech
