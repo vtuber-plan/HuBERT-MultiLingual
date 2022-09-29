b55a1f107770b13c5dc6318836bc1215ae0134e8
jstzwj88 <110.3870790@qq.com>
2022-09-29 16:57:40 +0800

    update multi node script

--- prepare.sh
+++ prepare.sh
@@ -18,13 +18,13 @@ python hubert/simple_kmeans/dump_mfcc_feature.py ${tsv_dir} test ${nshard} ${ran
 # python hubert/simple_kmeans/dump_hubert_feature.py ${tsv_dir} genshin_test ${ckpt_path} ${layer} ${nshard} ${rank} ${feat_dir}
 
 # k-means clustering
-python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} genshin_train ${nshard} km_train.joblib.pkl ${n_cluster} --percent 0.1
-python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} genshin_valid ${nshard} km_valid.joblib.pkl ${n_cluster} --percent 0.1
-python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} genshin_test ${nshard} km_test.joblib.pkl ${n_cluster} --percent 0.1
+python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} train ${nshard} km_train.joblib.pkl ${n_cluster} --percent 0.1
+python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} valid ${nshard} km_valid.joblib.pkl ${n_cluster} --percent 0.1
+python hubert/simple_kmeans/learn_kmeans.py ${feat_dir} test ${nshard} km_test.joblib.pkl ${n_cluster} --percent 0.1
 # k-means application
-python hubert/simple_kmeans/dump_km_label.py ${feat_dir} genshin_train km_train.joblib.pkl ${nshard} ${rank} ${lab_dir}
-python hubert/simple_kmeans/dump_km_label.py ${feat_dir} genshin_valid km_valid.joblib.pkl ${nshard} ${rank} ${lab_dir}
-python hubert/simple_kmeans/dump_km_label.py ${feat_dir} genshin_test km_test.joblib.pkl ${nshard} ${rank} ${lab_dir}
+python hubert/simple_kmeans/dump_km_label.py ${feat_dir} train km_train.joblib.pkl ${nshard} ${rank} ${lab_dir}
+python hubert/simple_kmeans/dump_km_label.py ${feat_dir} valid km_valid.joblib.pkl ${nshard} ${rank} ${lab_dir}
+python hubert/simple_kmeans/dump_km_label.py ${feat_dir} test km_test.joblib.pkl ${nshard} ${rank} ${lab_dir}
 
 for rank in $(seq 0 $((nshard - 1))); do
   cat $lab_dir/train_${rank}_${nshard}.km
